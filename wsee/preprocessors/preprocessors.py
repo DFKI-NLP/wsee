cb9c89bc589dcaeff8d80850d295194caa820eae
Phuc Tran Truong <ptruong@outlook.de>
2020-02-24 22:00:12 +0100

    Rewrite preprocessors functions using Snorkel preprocessor property

--- wsee/preprocessors/preprocessors.py
+++ wsee/preprocessors/preprocessors.py
@@ -1,54 +1,80 @@
-from typing import Optional
-
 from snorkel.preprocess import preprocessor
 from snorkel.types import DataPoint
 
 
-def get_index_for_id(obj_id, obj_list):
-    for idx, obj in enumerate(obj_list):
-        if obj['id'] == obj_id:
-            return idx
-    raise Exception(f'obj_id was not found in obj_list. The value of obj_id was: {obj_id}.\nThe obj_list: {obj_list}.')
-
-
-def get_entity(row, entity_id):
-    # future preprocessor function
-    entity_idx = get_index_for_id(entity_id, row.entities)
-    return row.entities[entity_idx]
-
-
-def get_entity_text(row, entity_id):
-    return get_entity(row, entity_id)['text']
-
-
-def get_entity_type(row, entity_id):
-    return get_entity(row, entity_id)['entity_type']
-
-
-def get_entity_left_tokens(row, entity_id, window_size: int = None):
-    if window_size is None:
-        window_start = 0
+def get_entity(entity_id, entities):
+    entity = next((x for x in entities if x['id'] == entity_id), None)
+    if entity is None:
+        raise Exception(f'The entity_id {entity_id} was not found in:\n {entities}')
     else:
-        window_start = max(0, entity_start - window_size)
-    window_end = get_entity(row, entity_id)['start']
-    return row.tokens[window_start:window_end]
+        return entity
 
 
-def get_entity_right_tokens(row, entity_id, window_size: int = None):
-    window_start = get_entity(row, entity_id)['end']
-    if window_size is None:
-        window_end = len(row.tokens)
+@preprocessor()
+def get_trigger(cand: DataPoint) -> DataPoint:
+    trigger = get_entity(cand.trigger_id, cand.entities)
+    cand.trigger = trigger
+    return cand
+
+
+@preprocessor()
+def get_argument(cand: DataPoint) -> DataPoint:
+    argument = get_entity(cand.argument_id, cand.entities)
+    cand.argument = argument
+    return cand
+
+
+@preprocessor()
+def get_left_tokens(cand: DataPoint) -> DataPoint:
+    trigger = get_entity(cand.trigger_id, cand.entities)
+    end = trigger['start']
+    cand.trigger_left_tokens = cand.tokens[0:end]
+
+    # Only relevant for event argument role classification
+    if 'argument_id' in cand:
+        argument = get_entity(cand.argument_id, cand.entities)
+        end = argument['start']
+        cand.argument_left_tokens = cand.tokens[0:end]
+    return cand
+
+
+@preprocessor()
+def get_right_tokens(cand: DataPoint) -> DataPoint:
+    trigger = get_entity(cand.trigger_id, cand.entities)
+    start = trigger['end']
+    cand.trigger_left_tokens = cand.tokens[start:]
+
+    # Only relevant for event argument role classification
+    if 'argument_id' in cand:
+        argument = get_entity(cand.argument_id, cand.entities)
+        start = argument['end']
+        cand.argument_left_tokens = cand.tokens[start:]
+    return cand
+
+
+@preprocessor()
+def get_between_tokens(cand: DataPoint) -> DataPoint:
+    trigger = get_entity(cand.trigger_id, cand.entities)
+    argument = get_entity(cand.argument_id, cand.entities)
+
+    # TODO: what if they overlap
+    if trigger['end'] <= argument['start']:
+        start = trigger['end']
+        end = argument['start']
     else:
-        window_end = min(len(row.tokens), window_start + window_size)
-    return row.tokens[window_start:window_end]
+        start = argument['end']
+        end = trigger['start']
+
+    cand.between_tokens = cand.tokens[start:end]
+    return cand
 
 
-def get_entity_distance(row, entity1_id, entity2_id):
-    entity1 = get_entity(row, entity1_id)
+def get_entity_distance(entities, entity1_id, entity2_id) -> int:
+    entity1 = get_entity(entity1_id, entities)
     entity1_start = entity1['start']
     entity1_end = entity1['end']
 
-    entity2 = get_entity(row, entity2_id)
+    entity2 = get_entity(entity2_id, entities)
     entity2_start = entity2['start']
     entity2_end = entity2['end']
     # TODO can entities overlap?
@@ -57,11 +83,5 @@ def get_entity_distance(row, entity1_id, entity2_id):
     elif entity2_end <= entity1_start:
         return entity1_start - entity2_end
     else:
-        print(f"WHAT THE HELL: overlapping entities {entity1} and {entity2}")
+        print(f"Overlapping entities {entity1} and {entity2}")
         return 0
-
-
-def get_trigger_arg_pair(row, trigger_id, argument_id):
-    trigger = get_entity(row, trigger_id)
-    argument = get_entity(row, argument_id)
-    return trigger, argument
