9f15c93d1463dba842e066510427315244bbfd1b
Phuc Tran Truong <ptruong@outlook.de>
2020-03-03 13:04:51 +0100

    Use last label in label_names if no negative label is given

--- wsee/utils/encode.py
+++ wsee/utils/encode.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 
-def one_hot_encode(label, label_names, negative_label='O'):
+def one_hot_encode(label, label_names, negative_label=None):
+    if negative_label is None:
+        negative_label = label_names[-1]
     label = label if label in label_names else negative_label
     class_probs = [1.0 if label_name == label else 0.0 for label_name in label_names]
     return class_probs
