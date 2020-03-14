6653f7de1e4d2ea92bd4efa6dab8882d145c64d0
Phuc Tran Truong <ptruong@outlook.de>
2020-03-14 18:52:12 +0100

    Add functions that merge event trigger/role examples if they belong to the same document

--- wsee/data/pipeline.py
+++ wsee/data/pipeline.py
@@ -7,6 +7,14 @@ from wsee.utils import utils
 
 
 def load_data(path, use_build_defaults=True):
+    """
+    Loads corpus data from specified path.
+    :param path: Path to corpus directory.
+    :param use_build_defaults: Whether to use data with defaults (trigger-entity pairs without
+    annotation in the data are assigned a negative label) or only the data where only the original
+    avro annotation was used
+    :return: output_dict containing train, dev, test, daystream data
+    """
     input_path = Path(path)
     assert input_path.exists(), 'Input not found: %s'.format(path)
 
@@ -29,8 +37,14 @@ def load_data(path, use_build_defaults=True):
 
 
 def build_event_trigger_examples(dataframe):
-    event_type_rows = []
-    event_type_rows_y = []
+    """
+    Takes a dataframe containing one document per row with all its annotations
+    (event triggers are of interest here) and creates one row for each event trigger.
+    :param dataframe: Annotated documents
+    :return: DataFrame containing event trigger examples and NumPy array containing labels
+    """
+    event_trigger_rows = []
+    event_trigger_rows_y = []
 
     event_count = 0
 
@@ -39,19 +53,26 @@ def build_event_trigger_examples(dataframe):
         for event_trigger in row.event_triggers:
             augmented_row = utils.get_deep_copy(row)
             augmented_row['trigger_id'] = event_trigger['id']
-            event_type_rows.append(augmented_row)
+            event_trigger_rows.append(augmented_row)
             event_type_num = np.asarray(event_trigger['event_type_probs']).argmax()
-            event_type_rows_y.append(event_type_num)
+            event_trigger_rows_y.append(event_type_num)
             if event_type_num != 7:
                 event_count += 1
 
     print("Number of events:", event_count)
-    event_type_rows = pd.DataFrame(event_type_rows)
-    event_type_rows_y = np.asarray(event_type_rows_y)
-    return event_type_rows, event_type_rows_y
+    event_trigger_rows = pd.DataFrame(event_trigger_rows)
+    event_trigger_rows_y = np.asarray(event_trigger_rows_y)
+    return event_trigger_rows, event_trigger_rows_y
 
 
-def build_event_roles_examples(dataframe):
+def build_event_role_examples(dataframe):
+    """
+    Takes a dataframe containing one document per row with all its annotations
+    (event roles are of interest here) and creates one row for each trigger-entity
+    (event role) pair.
+    :param dataframe: Annotated documents
+    :return: DataFrame containing event role examples and NumPy array containing labels
+    """
     event_role_rows_list = []
     event_role_rows_y = []
 
@@ -73,3 +94,77 @@ def build_event_roles_examples(dataframe):
     event_role_rows_y = np.asarray(event_role_rows_y)
 
     return event_role_rows, event_role_rows_y
+
+
+def build_labeled_event_trigger(x):
+    """
+    Builds event_trigger for example.
+    :param x: DataFrame row containing one event trigger example
+    :return: DataFrame row with filled event_triggers column
+    """
+    event_trigger = {
+        'id': x.trigger_id,
+        'event_type_probs': x.event_type_probs
+    }
+    x['event_triggers'] = [event_trigger]
+    return x
+
+
+def merge_event_trigger_examples(event_trigger_rows, event_trigger_probs):
+    """
+    Merges event trigger examples belonging to the same document.
+    :param event_trigger_rows: DataFrame containing the event trigger examples.
+    :param event_trigger_probs: NumPy array containing the event trigger class probabilities.
+    :return: DataFrame containing one document per row
+    """
+    # add event_trigger_probs to dataframe as additional column
+    event_trigger_rows['event_type_probs'] = list(event_trigger_probs)
+    event_trigger_rows = event_trigger_rows.apply(build_labeled_event_trigger, axis=1)
+    aggregation_functions = {
+        'text': 'first',
+        'tokens': 'first',
+        # 'pos_tags': 'first',
+        'ner_tags': 'first',
+        'entities': 'first',
+        'event_triggers': 'sum',  # expects list of one trigger per row
+        'event_roles': 'first'  # debatable
+
+    }
+    return event_trigger_rows.groupby('id').agg(aggregation_functions)
+
+
+def build_labeled_event_role(x):
+    """
+    Builds event_role for example.
+    :param x: DataFrame row containing one event role example
+    :return: DataFrame row with filled event_roles column
+    """
+    event_role = {
+        'trigger': x.trigger_id,
+        'argument': x.argument_id,
+        'event_argument_probs': x.event_argument_probs
+    }
+    x['event_roles'] = [event_role]
+    return x
+
+
+def merge_event_role_examples(event_role_rows, event_argument_probs):
+    """
+    Merges event role examples belonging to the same document.
+    :param event_role_rows: DataFrame containing the event role examples.
+    :param event_argument_probs: NumPy array containing the event role class probabilities.
+    :return: DataFrame containing one document per row
+    """
+    # add event_trigger_probs to dataframe as additional column
+    event_role_rows['event_argument_probs'] = list(event_argument_probs)
+    event_role_rows = event_role_rows.apply(build_labeled_event_role, axis=1)
+    aggregation_functions = {
+        'text': 'first',
+        'tokens': 'first',
+        # 'pos_tags': 'first',
+        'ner_tags': 'first',
+        'entities': 'first',
+        'event_triggers': 'first',  # debatable
+        'event_roles': 'sum'  # expects list of one event role per row
+    }
+    return event_role_rows.groupby('id').agg(aggregation_functions)
