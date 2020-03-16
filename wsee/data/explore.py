6631061b6dd02d47fe4bb0aa19951b0e29068697
Phuc Tran Truong <ptruong@outlook.de>
2020-03-16 12:28:40 +0100

    Add explore.py for data exploration

---
+++ wsee/data/explore.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from snorkel.preprocess import BasePreprocessor
+from wsee.preprocessors.preprocessors import *
+
+
+def add_labels(dataframe: pd.DataFrame, labels):
+    dataframe['label'] = list(labels)
+    return dataframe
+
+
+def apply_preprocessors(x: pd.DataFrame, pre: List[BasePreprocessor]):
+    """
+    Applies preprocessors to dataframe. (Essentially _preprocess_data_point function
+    from Snorkel's LabelingFunction class)
+    :param x: DataFrame.
+    :param pre: Preprocessors to run on data points.
+    :return:
+    """
+    for pre_func in pre:
+        x = pre_func(x)
+        if x is None:
+            raise ValueError("Preprocessor should not return None")
+    return x
