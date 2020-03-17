cdceac2104723dddd8a628a8604a0f9bdde7efd9
Phuc Tran Truong <ptruong@outlook.de>
2020-03-17 12:43:54 +0100

    Add test_apply_preprocessors function

--- tests/preprocessors/preprocessor_test.py
+++ tests/preprocessors/preprocessor_test.py
@@ -1,7 +1,8 @@
 import unittest
 
 import pandas as pd
-from wsee.preprocessors.preprocessors import get_mixed_ner
+from wsee.preprocessors import preprocessors
+from wsee.data import pipeline, explore
 
 
 class TestMixedNer(unittest.TestCase):
@@ -16,8 +17,9 @@ class TestMixedNer(unittest.TestCase):
 
             actual_cand = gold_cand.drop(columns=['mixed_ner'])
 
-            actual_cand = get_mixed_ner(actual_cand)
-            self.assertEqual(expected_mixed_ner, actual_cand['mixed_ner'])
+            actual_cand = preprocessors.get_mixed_ner(actual_cand)
+            # TODO: check trailing whitespace/ newline that is somehow introduced in preprocessor?
+            self.assertEqual(expected_mixed_ner.strip(), actual_cand['mixed_ner'].strip())
 
     def test_mixed_ner_spans(self):
         for index, gold_cand in self.pd_df.iterrows():
@@ -27,8 +29,14 @@ class TestMixedNer(unittest.TestCase):
 
             actual_cand = gold_cand.drop(columns=['mixed_ner_spans'])
 
-            actual_cand = get_mixed_ner(actual_cand)
+            actual_cand = preprocessors.get_mixed_ner(actual_cand)
             self.assertEqual(expected_mixed_ner_spans, actual_cand['mixed_ner_spans'])
+
+    def test_apply_preprocessors(self):
+        event_type_rows, event_type_rows_y = pipeline.build_event_trigger_examples(self.pd_df)
+        labeled_rows = explore.add_labels(event_type_rows, event_type_rows_y)
+        processed_rows = explore.apply_preprocessors(labeled_rows, pre=[preprocessors.get_trigger])
+        self.assertIsNotNone(processed_rows)
 
 
 if __name__ == '__main__':
