24af2deedd1b7db2d48e3d9739360dfaafca7371
Phuc Tran Truong <ptruong@outlook.de>
2020-04-03 15:29:06 +0200

    Add gazetteer parser

--- wsee/utils/utils.py
+++ wsee/utils/utils.py
@@ -1,5 +1,6 @@
 import pickle
 import json
+import re
 
 
 def get_deep_copy(obj):
@@ -8,3 +9,15 @@ def get_deep_copy(obj):
 
 def pretty_print_json(obj):
     print(json.dumps(json.loads(obj.to_json()), indent=2, ensure_ascii=False))
+
+
+def parse_gaz_file(path):
+    cause_consequence_mapping = {}
+    with open(path, 'r') as gaz_reader:
+        for line in gaz_reader.readlines():
+            cols = line.split(' | ')
+            cause_event = cols[0]
+            consequence = re.findall('"([^"]*)"', cols[2])
+            assert len(consequence) > 0
+            cause_consequence_mapping[cause_event] = consequence[0]
+    return cause_consequence_mapping
