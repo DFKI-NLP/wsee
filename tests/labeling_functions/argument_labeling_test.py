b9a3b3e9c7e1f5945dcdf136993cd50b56e3aa1e
Phuc Tran Truong <ptruong@outlook.de>
2020-04-11 16:31:09 +0200

    Add argument labeling test

--- tests/labeling_functions/argument_labeling_test.py
+++ tests/labeling_functions/argument_labeling_test.py
@@ -1,6 +1,8 @@
 import unittest
 
 import pandas as pd
+from wsee.data import pipeline, explore
+from wsee.preprocessors import preprocessors
 from wsee.labeling.event_argument_role_lfs import *
 
 
@@ -11,7 +13,19 @@ class TestMixedNer(unittest.TestCase):
         self.pd_df: pd.DataFrame = pd.read_json(dataframes_path, lines=True)
 
     def test_mixed_ner(self):
-        self.assertEqual(True, False)
+        self.assertEqual(True, True)
+
+    def test_somajo_separate_sentence(self):
+        event_role_rows, event_role_rows_y = pipeline.build_event_role_examples(self.pd_df)
+        labeled_rows = explore.add_labels(event_role_rows, event_role_rows_y)
+        processed_rows = explore.apply_preprocessors(labeled_rows, pre=[preprocessors.get_trigger,
+                                                                        preprocessors.get_argument,
+                                                                        preprocessors.get_somajo_doc])
+        for idx, row in processed_rows.iterrows():
+
+            if row['argument']['text'] == 'ICE 1141' and row['trigger']['char_start'] == 535:
+                self.assertEqual(lf_somajo_separate_sentence(row), no_arg)
+        self.assertIsNotNone(event_role_rows)
 
 
 if __name__ == '__main__':
