20ecb92130c791beac11093653b437301eacac8d
Phuc Tran Truong <ptruong@outlook.de>
2020-04-16 21:23:10 +0200

    Remove trigger text counts

--- wsee/labeling/error_analysis.py
+++ wsee/labeling/error_analysis.py
@@ -15,11 +15,6 @@ def sample_fp(labeled_df: pd.DataFrame, lf_outputs: np.ndarray, lf_index: int, l
     return explore.sample_data(false_positives)
 
 
-def trigger_text_counts_fp(labeled_df: pd.DataFrame, lf_outputs: np.ndarray, lf_index: int, label_of_interest: int):
-    false_positives = get_false_positives(labeled_df, lf_outputs, lf_index, label_of_interest)
-    return false_positives['trigger_text'].value_counts()
-
-
 def get_abstained_instances(labeled_df: pd.DataFrame, lf_outputs: np.ndarray, lf_index: int):
     abstains = labeled_df.iloc[lf_outputs[:, lf_index] == -1]
     return abstains
@@ -28,8 +23,3 @@ def get_abstained_instances(labeled_df: pd.DataFrame, lf_outputs: np.ndarray, lf
 def sample_abstained_instances(labeled_df: pd.DataFrame, lf_outputs: np.ndarray, lf_index: int, label_of_interest: int):
     abstains = get_abstained_instances(labeled_df, lf_outputs, lf_index)
     return explore.sample_data(abstains[abstains['label'] == label_of_interest])
-
-
-def trigger_text_counts_ai(labeled_df: pd.DataFrame, lf_outputs: np.ndarray, lf_index: int, label_of_interest: int):
-    abstains = get_abstained_instances(labeled_df, lf_outputs, lf_index)
-    return abstains[abstains['label'] == label_of_interest]['trigger_text'].value_counts()
