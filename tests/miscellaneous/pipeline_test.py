dff33d4029bc673812365fe7b3d77a14ff9428ed
Phuc Tran Truong <ptruong@outlook.de>
2020-04-24 11:31:30 +0200

    Update class balance for event roles, update pipeline test

--- tests/miscellaneous/pipeline_test.py
+++ tests/miscellaneous/pipeline_test.py
@@ -15,20 +15,20 @@ class TestPipeline(unittest.TestCase):
     def test_event_triggers(self):
         event_type_rows, event_type_rows_y = pipeline.build_event_trigger_examples(self.pd_df)
         event_trigger_examples = utils.get_deep_copy(event_type_rows)
-        self.assertTrue(len(event_trigger_examples) == 3)
+        self.assertEqual(9, len(event_trigger_examples))
 
         trigger_class_probs = np.random.rand(len(event_trigger_examples), 3)
         merged_trigger_examples = pipeline.merge_event_trigger_examples(event_trigger_examples, trigger_class_probs)
-        self.assertTrue(len(merged_trigger_examples) == 2)
+        self.assertEqual(4, len(merged_trigger_examples))
 
     def test_event_roles(self):
         event_role_rows, event_role_rows_y = pipeline.build_event_role_examples(self.pd_df)
         event_role_examples = utils.get_deep_copy(event_role_rows)
-        self.assertTrue(len(event_role_examples) == 11)
+        self.assertEqual(165, len(event_role_examples))
 
         role_class_probs = np.random.rand(len(event_role_examples), 3)
         merged_role_examples = pipeline.merge_event_role_examples(event_role_examples, role_class_probs)
-        self.assertTrue(len(merged_role_examples) == 2)
+        self.assertEqual(4, len(merged_role_examples))
 
     def test_build_training_data(self):
         merged_examples = pipeline.build_training_data(self.pd_df)
