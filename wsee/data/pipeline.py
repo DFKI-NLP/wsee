f5a8da13690bdec6c1dcd9d398b7d67d072f9fdc
Phuc Tran Truong <ptruong@outlook.de>
2020-06-20 13:34:14 +0200

    Use optional Y_dev to set class balance priors instead of manually estimated class balance based on coverage of labeling functions
    - Update labeling function names
    - Filter documents with no events/ just abstains

--- wsee/data/pipeline.py
+++ wsee/data/pipeline.py
@@ -68,10 +68,11 @@ def load_data(path, use_build_defaults=True):
     return output_dict
 
 
-def build_event_trigger_examples(dataframe):
+def build_event_trigger_examples(dataframe, preprocess=True):
     """
     Takes a dataframe containing one document per row with all its annotations
     (event triggers are of interest here) and creates one row for each event trigger.
+    :param preprocess: Whether to do any preprocessing during the build.
     :param dataframe: Annotated documents.
     :return: DataFrame containing event trigger examples and NumPy array containing labels.
     """
@@ -84,11 +85,12 @@ def build_event_trigger_examples(dataframe):
     logging.info("Building event trigger examples")
     logging.info(f"DataFrame has {len(dataframe.index)} rows")
     for index, row in tqdm(dataframe.iterrows()):
-        entity_type_freqs = preprocessors.get_entity_type_freqs(row)
+        entity_type_freqs = preprocessors.get_entity_type_freqs(row) if preprocess else None
         for event_trigger in row.event_triggers:
             trigger_row = row.copy()
             trigger_row['trigger'] = preprocessors.get_entity(event_trigger['id'], row.entities)
-            trigger_row['entity_type_freqs'] = entity_type_freqs
+            if preprocess:
+                trigger_row['entity_type_freqs'] = entity_type_freqs
             event_trigger_rows.append(trigger_row)
             event_type_num = np.asarray(event_trigger['event_type_probs']).argmax()
             event_trigger_rows_y.append(event_type_num)
@@ -112,12 +114,13 @@ def arg_location_type_event_type_match(cand):
     return False
 
 
-def build_event_role_examples(dataframe):
+def build_event_role_examples(dataframe, preprocess=True):
     """
     Takes a dataframe containing one document per row with all its annotations
     (event roles are of interest here) and creates one row for each trigger-entity
     (event role) pair. Also adds attributes beforehand instead of using preprocessors in
     order not to do it for each row or even each row*labeling functions.
+    :param preprocess: Whether to do any preprocessing during the build.
     :param dataframe: Annotated documents.
     :return: DataFrame containing event role examples and NumPy array containing labels.
     """
@@ -133,22 +136,25 @@ def build_event_role_examples(dataframe):
                  "entity_type_freqs, somajo_doc, mixed_ner, mixed_ner_spans, not_an_event, arg_type_event_type_match, "
                  "between_distance, is_multiple_same_event_type")
     for index, row in tqdm(dataframe.iterrows()):
-        entity_type_freqs = preprocessors.get_entity_type_freqs(row)
-        somajo_doc = preprocessors.get_somajo_doc(row)
-        mixed_ner, mixed_ner_spans = preprocessors.get_mixed_ner(row)
+        entity_type_freqs, somajo_doc, mixed_ner, mixed_ner_spans = None, None, None, None
+        if preprocess:
+            entity_type_freqs = preprocessors.get_entity_type_freqs(row)
+            somajo_doc = preprocessors.get_somajo_doc(row)
+            mixed_ner, mixed_ner_spans = preprocessors.get_mixed_ner(row)
         for event_role in row.event_roles:
             role_row = row.copy()
             role_row['trigger'] = preprocessors.get_entity(event_role['trigger'], row.entities)
             role_row['argument'] = preprocessors.get_entity(event_role['argument'], row.entities)
-            role_row['entity_type_freqs'] = entity_type_freqs
-            role_row['somajo_doc'] = somajo_doc
-            role_row['separate_sentence'] = preprocessors.get_somajo_separate_sentence(role_row)
-            role_row['mixed_ner'] = mixed_ner
-            role_row['mixed_ner_spans'] = mixed_ner_spans
-            role_row['not_an_event'] = trigger_lfs.lf_negative(role_row) == trigger_lfs.O
-            role_row['arg_location_type_event_type_match'] = arg_location_type_event_type_match(role_row)
-            role_row['between_distance'] = preprocessors.get_between_distance(role_row)
-            role_row['is_multiple_same_event_type'] = preprocessors.is_multiple_same_event_type(role_row)
+            if preprocess:
+                role_row['entity_type_freqs'] = entity_type_freqs
+                role_row['somajo_doc'] = somajo_doc
+                role_row['separate_sentence'] = preprocessors.get_somajo_separate_sentence(role_row)
+                role_row['mixed_ner'] = mixed_ner
+                role_row['mixed_ner_spans'] = mixed_ner_spans
+                role_row['not_an_event'] = trigger_lfs.lf_negative(role_row) == trigger_lfs.O
+                role_row['arg_location_type_event_type_match'] = arg_location_type_event_type_match(role_row)
+                role_row['between_distance'] = preprocessors.get_between_distance(role_row)
+                role_row['is_multiple_same_event_type'] = preprocessors.is_multiple_same_event_type(role_row)
             event_role_rows_list.append(role_row)
             event_role_num = np.asarray(event_role['event_argument_probs']).argmax()
             event_role_rows_y.append(event_role_num)
@@ -242,28 +248,31 @@ def merge_event_role_examples(event_role_rows: pd.DataFrame, event_argument_prob
     return event_role_rows.groupby('id').agg(aggregation_functions)
 
 
-def get_trigger_probs(l_train: pd.DataFrame, filter_abstains: bool = True,
+def get_trigger_probs(lf_train: pd.DataFrame, filter_abstains: bool = False,
                       lfs: Optional[List[labeling_function]] = None,
-                      label_model: LabelModel = None):
+                      lf_dev: pd.DataFrame = None):
     """
     Takes "raw" data frame, builds trigger examples, (trains LabelModel), calculates event_trigger_probs
     and returns merged trigger examples with event_trigger_probs.
     :param filter_abstains: Filters rows where all labeling functions abstained
-    :param l_train:
-    :param lfs:
-    :param label_model:
+    :param lf_train: Training dataset which will be labeled using Snorkel
+    :param lfs: List of labeling functions
+    :param lf_dev: Optional development dataset that can be used to set a prior for the class balance
     :return:
     """
-    df_train, _ = build_event_trigger_examples(l_train)
+    df_train, _ = build_event_trigger_examples(lf_train)
+    Y_dev = None
+    if lf_dev:
+        _, Y_dev = build_event_trigger_examples(lf_dev, preprocess=False)
     if lfs is None:
         lfs = [
             trigger_lfs.lf_accident_context,
             trigger_lfs.lf_accident_context_street,
             trigger_lfs.lf_accident_context_no_cause_check,
             trigger_lfs.lf_canceledroute_cat,
-            trigger_lfs.lf_canceledroute_amplifier1,
+            trigger_lfs.lf_canceledroute_replicated,
             trigger_lfs.lf_canceledstop_cat,
-            trigger_lfs.lf_canceledstop_amplifier1,
+            trigger_lfs.lf_canceledstop_replicated,
             trigger_lfs.lf_delay_cat,
             trigger_lfs.lf_delay_priorities,
             trigger_lfs.lf_delay_duration,
@@ -271,7 +280,7 @@ def get_trigger_probs(l_train: pd.DataFrame, filter_abstains: bool = True,
             trigger_lfs.lf_obstruction_street,
             trigger_lfs.lf_obstruction_priorities,
             trigger_lfs.lf_railreplacementservice_cat,
-            trigger_lfs.lf_railreplacementservice_amplifier1,
+            trigger_lfs.lf_railreplacementservice_replicated,
             trigger_lfs.lf_trafficjam_cat,
             trigger_lfs.lf_trafficjam_street,
             trigger_lfs.lf_trafficjam_order,
@@ -282,20 +291,9 @@ def get_trigger_probs(l_train: pd.DataFrame, filter_abstains: bool = True,
     logging.info("Running Event Trigger Labeling Function Applier")
     applier = PandasLFApplier(lfs)
     L_train = applier.apply(df_train)
-    logging.info("Fitting LabelModel on the data and predicting class probabilities")
-    if label_model is None:
-        label_model = LabelModel(cardinality=8, verbose=True)
-        label_model.fit(L_train=L_train, n_epochs=500, log_freq=100, seed=123,
-                        class_balance=[
-                            0.05769230769230769,
-                            0.24038461538461536,
-                            0.028846153846153844,
-                            0.2019230769230769,
-                            0.19230769230769232,
-                            0.09615384615384616,
-                            0.028846153846153844,
-                            0.15384615384615385]
-                        )
+    logging.info("Fitting LabelModel on the data and predicting trigger class probabilities")
+    label_model = LabelModel(cardinality=8, verbose=True)
+    label_model.fit(L_train=L_train, n_epochs=500, log_freq=100, seed=123, Y_dev=Y_dev)
     event_trigger_probs = label_model.predict_proba(L_train)
 
     if filter_abstains:
@@ -305,21 +303,25 @@ def get_trigger_probs(l_train: pd.DataFrame, filter_abstains: bool = True,
 
         return merge_event_trigger_examples(df_train_filtered, probs_train_filtered)
     else:
+        # Multiplies probabilities of abstains with zero so that the example is treated as padding in the end model
         return merge_event_trigger_examples(df_train, utils.zero_out_abstains(event_trigger_probs, L_train))
 
 
-def get_role_probs(l_train: pd.DataFrame, filter_abstains: bool = True,
+def get_role_probs(lf_train: pd.DataFrame, filter_abstains: bool = False,
                    lfs: Optional[List[labeling_function]] = None,
-                   label_model: LabelModel = None):
+                   lf_dev: pd.DataFrame = None):
     """
 
     :param filter_abstains: Filters rows where all labeling functions abstained
-    :param l_train:
-    :param lfs:
-    :param label_model:
+    :param lf_train: Training dataset which will be labeled using Snorkel
+    :param lfs: List of labeling functions
+    :param lf_dev: Optional development dataset that can be used to set a prior for the class balance
     :return:
     """
-    df_train, _ = build_event_role_examples(l_train)
+    df_train, _ = build_event_role_examples(lf_train)
+    Y_dev = None
+    if lf_dev:
+        _, Y_dev = build_event_role_examples(lf_dev, preprocess=False)
     if lfs is None:
         lfs = [
             role_lfs.lf_location_adjacent_markers,
@@ -341,13 +343,13 @@ def get_role_probs(l_train: pd.DataFrame, filter_abstains: bool = True,
             role_lfs.lf_end_location_nearest,
             role_lfs.lf_end_location_preceding_arg,
             role_lfs.lf_start_date_type,
-            role_lfs.lf_start_date_amplifier,
+            role_lfs.lf_start_date_replicated,
             role_lfs.lf_start_date_first,
             role_lfs.lf_start_date_adjacent,
             role_lfs.lf_end_date_type,
-            role_lfs.lf_end_date_amplifier,
+            role_lfs.lf_end_date_replicated,
             role_lfs.lf_cause_type,
-            role_lfs.lf_cause_amplifier,
+            role_lfs.lf_cause_replicated,
             role_lfs.lf_cause_order,
             role_lfs.lf_distance_type,
             role_lfs.lf_distance_nearest,
@@ -368,23 +370,9 @@ def get_role_probs(l_train: pd.DataFrame, filter_abstains: bool = True,
     logging.info("Running Event Role Labeling Function Applier")
     applier = PandasLFApplier(lfs)
     L_train = applier.apply(df_train)
-    logging.info("Fitting LabelModel on the data and predicting class probabilities")
-    if label_model is None:
-        label_model = LabelModel(cardinality=11, verbose=True)
-        label_model.fit(L_train=L_train, n_epochs=500, log_freq=100, seed=123,
-                        class_balance=[
-                            0.043066322136089574,
-                            0.00717772035601493,
-                            0.00717772035601493,
-                            0.018662072925638817,
-                            0.011484352569623888,
-                            0.011484352569623888,
-                            0.002871088142405972,
-                            0.005742176284811944,
-                            0.0008613264427217915,
-                            0.001435544071202986,
-                            0.8900373241458512
-                        ])
+    logging.info("Fitting LabelModel on the data and predicting role class probabilities")
+    label_model = LabelModel(cardinality=11, verbose=True)
+    label_model.fit(L_train=L_train, n_epochs=500, log_freq=100, seed=123, Y_dev=Y_dev)
     event_role_probs = label_model.predict_proba(L_train)
 
     if filter_abstains:
@@ -394,20 +382,25 @@ def get_role_probs(l_train: pd.DataFrame, filter_abstains: bool = True,
 
         return merge_event_role_examples(df_train_filtered, probs_train_filtered)
     else:
+        # Multiplies probabilities of abstains with zero so that the example is treated as padding in the end model
         return merge_event_role_examples(df_train, utils.zero_out_abstains(event_role_probs, L_train))
 
 
-def build_training_data(lf_train: pd.DataFrame, save_path=None, sample=False) -> pd.DataFrame:
+def build_training_data(lf_train: pd.DataFrame, save_path=None, sample=False,
+                        lf_dev: pd.DataFrame = None) -> pd.DataFrame:
     """
     Merges event_trigger_examples and event_role examples to build training data.
     :param sample: When set to true, only use a sample of the data.
     :param save_path: Where to save the dataframe as a jsonl
     :param lf_train: DataFrame with original data.
+    :param lf_dev: DataFrame with gold labels, which can be used to estimate the class balance for triggers & roles
     :return: Original DataFrame updated with event triggers and event roles.
     """
     if sample and len(lf_train) > 100:
         lf_train = lf_train.sample(100)
-    merged_event_trigger_examples = get_trigger_probs(lf_train)
+
+    # Trigger labeling
+    merged_event_trigger_examples = get_trigger_probs(lf_train=lf_train, lf_dev=lf_dev)
 
     if save_path:
         try:
@@ -417,9 +410,8 @@ def build_training_data(lf_train: pd.DataFrame, save_path=None, sample=False) ->
         except Exception as e:
             print(e)
 
-    # TODO: use event trigger information from trigger examples for role labeling
-
-    merged_event_role_examples = get_role_probs(lf_train)
+    # Role labeling
+    merged_event_role_examples = get_role_probs(lf_train=lf_train, lf_dev=lf_dev)
 
     if save_path:
         try:
@@ -429,8 +421,9 @@ def build_training_data(lf_train: pd.DataFrame, save_path=None, sample=False) ->
         except Exception as e:
             print(e)
 
+    # Merge
     merged_examples: pd.DataFrame = utils.get_deep_copy(lf_train)
-    # Make sure to remove event_triggers and roles that were built per default
+    # Make sure to remove event_triggers and roles that were built per default during the avro-json conversion
     for idx, row in merged_examples.iterrows():
         row['event_triggers'] = []
         row['event_roles'] = []
@@ -444,23 +437,16 @@ def build_training_data(lf_train: pd.DataFrame, save_path=None, sample=False) ->
 
     merged_examples.reset_index(level=0, inplace=True)
 
-    # Removes rows with no events
-    merged_examples = merged_examples[merged_examples['event_triggers'].map(lambda d: len(d)) > 0]
-
-    # # Create additional version where the most probable class gets probability 1.0
-    # dominated_examples: pd.DataFrame = utils.get_deep_copy(merged_examples)
-    # dominated_examples.apply(utils.let_most_probable_class_dominate, axis=1)
+    # Removes rows with no events/ no positively labeled events
+    examples_with_events = merged_examples.apply(
+        lambda document: utils.has_events(document, include_negatives=True), axis=1)
+    merged_examples = merged_examples[examples_with_events]
 
     if save_path:
         try:
             logging.info(f"Writing Snorkel Labeled data to {save_path + '/daystream_snorkeledv6_pipeline.jsonl'}")
             merged_examples.to_json(
                 save_path + '/daystream_snorkeledv6_pipeline.jsonl', orient='records', lines=True, force_ascii=False)
-            # logging.info(f"Writing Snorkel Labeled data where probability of most probable class is set to 1.0 "
-            #              f"to {save_path + '/daystream_snorkeledv6_pipeline_dominated.jsonl'}")
-            # dominated_examples.to_json(
-            #     save_path + '/daystream_snorkeledv6_pipeline_dominated.jsonl', orient='records', lines=True,
-            #     force_ascii=False)
         except Exception as e:
             print(e)
     return merged_examples
